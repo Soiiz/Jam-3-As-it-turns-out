e9dc850a1e2ac7a621bfcdca58fcfb6ae96cfccd
Soiiz <kchau6@ucsc.edu>
2022-11-30 19:25:36 -0800

    Enemy healthbar added

---
+++ Scripts/EnemyHealthBar.gd
@@ -0,0 +1,4 @@
+extends ProgressBar
+
+func _on_Interface_enemy_HP_Updated(health):
+	value = health
--- Scripts/Interface.gd
+++ Scripts/Interface.gd
@@ -2,7 +2,11 @@ extends Control
 
 
 signal health_changed(hp)
-
+signal enemy_HP_Updated(health)
 
 func _on_Player_health_changed(hp):
 	emit_signal("health_changed", hp)
+
+
+func _on_Enemy_enemy_HP_Updated(health):
+	emit_signal("enemy_HP_Updated", health)
